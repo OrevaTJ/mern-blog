fa4cbfba7274be30707070f8c9ed8a11b0fb48ff
Oreva <khemical51@gmail.com>
2024-02-26 11:08:20 +0100

    create routes and error middleware

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config();
 
@@ -15,6 +17,22 @@ mongoose
 
 const app = express();
 
+app.use(express.json())
+
 app.listen(3001, () => {
   console.log('Server running on port 3001');
 });
+
+app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
+
+app.use((error, req, res, next) => {
+    const statusCode = error.statusCode || 500;
+    const message = error.message || 'Internal server error';
+  
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
