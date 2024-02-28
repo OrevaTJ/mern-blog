8ce4f3c334ccb9c362bbe4bbb94b52eeb99edbe8
Oreva <khemical51@gmail.com>
2024-02-28 15:03:03 +0100

    add user update reducer

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -21,10 +21,31 @@ export const userSlice = createSlice({
     signInError: (state, action) => {
       (state.error = action.payload), (state.loading = false);
     },
+
+    // update reducers
+    updateUserStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateUserSuccess: (state, action) => {
+      (state.currentUser = action.payload),
+        (state.loading = false),
+        (state.error = null);
+    },
+    updateUserFailure: (state, action) => {
+      (state.error = action.payload), (state.loading = false);
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { signInStart, signInSuccess, signInError } = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInError,
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+} = userSlice.actions;
 
 export default userSlice.reducer;
