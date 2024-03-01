cec79d3e7c19abcecce0666f7ab607723c08aca6
Oreva <khemical51@gmail.com>
2024-03-01 21:00:26 +0100

    add dashboard page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,37 @@
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import DashboardSidebar from '../components/dashboard/DashboardSidebar'
+import DashboardProfile from '../components/dashboard/DashboardProfile'
+import DashboardUsers from '../components/dashboard/DashboardUsers'
+import DashboardPosts from '../components/dashboard/DashboardPosts'
+import DashboardComments from '../components/dashboard/DashboardComments'
 
 export default function Dashboard() {
+  const location = useLocation()
+  const [tab, setTab] = useState(null)
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const urlTab = urlParams.get('tab')
+    urlTab && setTab(urlTab)
+  }, [location.search])
+
   return (
-    <div>Dashboard</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        {/* Sidebar */}
+        <DashboardSidebar />
+      </div>
+      {/* profile... */}
+      {tab === 'profile' && <DashboardProfile />}
+      {/* posts... */}
+      {tab === 'posts' && <DashboardPosts />}
+      {/* users */}
+      {tab === 'users' && <DashboardUsers />}
+      {/* comments  */}
+      {tab === 'comments' && <DashboardComments />}
+      {/* dashboard comp */}
+      {/* {tab === 'dash' && <DashboardComp />} */}
+    </div>
   )
 }
