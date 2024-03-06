9b069b95d1cb6cb054811dc6fec40e48a9e0c75d
Oreva <khemical51@gmail.com>
2024-03-06 20:21:02 +0100

    like comment route

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { create, getComments } from '../controllers/comment.controller.js';
+import { create, getComments, editComment } from '../controllers/comment.controller.js';
 import { verifyToken } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, create);
-router.post('/:postId', getComments);
+router.put('/likeComment/:commentId', verifyToken, likeComment);
+router.get('/:postId', getComments);
 
 export default router;
