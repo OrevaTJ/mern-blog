8367b151d976ae89ee152fb74069b46fe594bfda
Oreva <khemical51@gmail.com>
2024-03-06 20:20:19 +0100

    like a comment

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -31,3 +31,33 @@ export const getComments = async (req, res, next) => {
     next(error);
   }
 };
+
+export const likeComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.commentId);
+
+    if (!comment) {
+      return next(errorHandler(404, 'Comment not found'));
+    }
+
+    // Check if the user has already liked the comment
+    const alreadyLikedIndex = comment.likes.findIndex(
+      (like) => like.user.toString() === req.user.id
+    );
+
+    if (alreadyLikedIndex !== -1) {
+      // User has already liked the comment, so unlike it
+      comment.likes.splice(alreadyLikedIndex, 1);
+      comment.numberOfLikes--;
+    } else {
+      // User hasn't liked the comment, so like it
+      comment.likes.push(req.user.id); 
+      comment.numberOfLikes++; 
+    }
+
+    await comment.save();
+    res.status(200).json(comment);
+  } catch (error) {
+    next(error);
+  }
+};
