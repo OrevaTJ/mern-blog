39c7af16f12d634afd97b24e4c946bc94af5d7f6
Oreva <khemical51@gmail.com>
2024-03-06 20:48:11 +0100

    api route to get single user

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { updateUser, deleteUser, signout, getUsers } from "../controllers/user.controller.js";
+import { updateUser, deleteUser, signout, getUsers, getSingleUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router()
@@ -8,5 +8,6 @@ router.put('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id',verifyToken, deleteUser)
 router.post('/signout', signout);
 router.get('/', verifyToken, getUsers);
+router.get('/:userId', getSingleUser);
 
 export default router
