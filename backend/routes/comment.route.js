d159c35377397ad5a1056f72f9f5a91a7c01fea6
Oreva <khemical51@gmail.com>
2024-03-07 21:59:33 +0100

    delete comment api route

--- backend/routes/comment.route.js
+++ backend/routes/comment.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { create, getComments, editComment } from '../controllers/comment.controller.js';
+import { create, getComments, editComment, deleteComment, likeComment } from '../controllers/comment.controller.js';
 import { verifyToken } from '../utils/verifyToken.js';
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post('/create', verifyToken, create);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
 router.get('/:postId', getComments);
 
 export default router;
