a7ed61546d35f0ceee0fbd1ddf3454819510c35c
Oreva <khemical51@gmail.com>
2024-03-11 10:57:03 +0100

    about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,6 +1,36 @@
-
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About GiTi Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis ad
+              natus placeat debitis accusamus at nobis, ratione molestias
+              commodi nisi autem rem odio itaque nemo vitae unde perspiciatis
+              veniam consectetur pariatur consequatur nulla eveniet ea? Atque
+              quos vel doloribus facilis.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis ad
+              natus placeat debitis accusamus at nobis, ratione molestias
+              commodi nisi autem rem odio itaque nemo vitae unde perspiciatis
+              veniam consectetur pariatur consequatur nulla eveniet ea? Atque
+              quos vel doloribus facilis.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis ad
+              natus placeat debitis accusamus at nobis, ratione molestias
+              commodi nisi autem rem odio itaque nemo vitae unde perspiciatis
+              veniam consectetur pariatur consequatur nulla eveniet ea? Atque
+              quos vel doloribus facilis.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
