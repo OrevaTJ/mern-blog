f8cb0613d3f161cef03d189f1793b6a3326af35f
Oreva <khemical51@gmail.com>
2024-03-11 11:04:15 +0100

    add pre-build update

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import userRoutes from './routes/user.route.js'
-import authRoutes from './routes/auth.route.js'
-import postRoutes from './routes/post.route.js'
-import commentRoutes from './routes/comment.route.js'
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/comment.route.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -18,27 +19,35 @@ mongoose
     console.log(error);
   });
 
+const __dirname = path.resolve();
+
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3001, () => {
   console.log('Server running on port 3001');
 });
 
-app.use('/api/user', userRoutes)
-app.use('/api/auth', authRoutes)
-app.use('/api/post', postRoutes)
-app.use('/api/comment', commentRoutes)
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((error, req, res, next) => {
-    const statusCode = error.statusCode || 500;
-    const message = error.message || 'Internal server error';
-  
-    return res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
-    });
+  const statusCode = error.statusCode || 500;
+  const message = error.message || 'Internal server error';
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
   });
+});
