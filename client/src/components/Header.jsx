57f0a2ccb87e951617a56b83415d7e14ebfe666a
Oreva <khemical51@gmail.com>
2024-03-18 09:08:29 +0100

    update code

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -34,16 +34,17 @@ export default function Header() {
   };
 
   return (
-    <Navbar className="border-b-2">
+    <Navbar>
       <Link
         to="/"
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+        <span className="px-2 py-1 bg-gradient-to-r from-black via-gray-600 to-pink-500 rounded-lg text-white">
           GiTi
         </span>
         Blog
       </Link>
+
       <form onSubmit={handleSubmit}>
         <TextInput
           type="text"
@@ -54,18 +55,19 @@ export default function Header() {
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
-      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
-        <AiOutlineSearch />
-      </Button>
-      <div className="flex gap-2 md:order-2">
-        <Button
-          className="w-12 h-10 hidden sm:inline"
-          color="gray"
-          pill
+
+      <div className="flex items-center gap-2 md:order-2">
+        <Button className="w-8 h-8 lg:hidden" color="gray" pill>
+          <AiOutlineSearch />
+        </Button>
+
+        <button
+          className="md:flex items-center justify-center px-1 py-1 bg-slate-200 dark:bg-slate-600 rounded-full hidden"
           onClick={() => dispatch(toggleTheme())}
         >
           {theme === 'light' ? <FaMoon /> : <FaSun />}
-        </Button>
+        </button>
+
         {currentUser ? (
           <Dropdown
             arrowIcon={false}
@@ -93,11 +95,22 @@ export default function Header() {
             </Button>
           </Link>
         )}
+
         <Navbar.Toggle />
       </div>
+
       <Navbar.Collapse>
-        <Navbar.Link active={path === '/'} as={'div'}>
+        <Navbar.Link active={path === '/'} as={'div'} className='flex justify-between'>
           <Link to="/">Home</Link>
+          <button
+            className="flex items-center justify-center px-1 py-1 dark:bg-slate-600 rounded-full md:hidden"
+            onClick={() => dispatch(toggleTheme())}
+          >
+            {theme === 'light' ? <FaMoon /> : <FaSun />}
+          </button>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/tags'} as={'div'}>
+          <Link to="/tags">Tags</Link>
         </Navbar.Link>
         <Navbar.Link active={path === '/about'} as={'div'}>
           <Link to="/about">About</Link>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
-import CallToAction from '../components/CallToAction';
 import { useEffect, useState } from 'react';
-import PostCard from '../components/PostCard';
+import { fullNameDate } from '../utils/dateUtils';
+import { insertPTag } from '../utils/insertHTML';
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
@@ -16,34 +16,80 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
-      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto ">
-        <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my Blog</h1>
-        <p className="text-gray-500 text-xs sm:text-sm">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae quidem
-          nostrum fuga maiores qui deleniti dicta libero est doloremque repellat
-          ex ad pariatur dolore vero reiciendis itaque, aperiam, delectus iusto.
-        </p>
-        <Link
-          to="/search"
-          className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
+    <div className="mx-2 md:mx-6 min-h-screen">
+      {posts.length > 0 && (
+        <div
+          style={{
+            backgroundImage: `url(${posts[0].image})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+          }}
+          className="h-52 md:h-[400px] flex align-middle my-4"
         >
-          View all posts
-        </Link>
-      </div>
-      <div className="p-3 bg-amber-100 dark:bg-slate-700">
-        <CallToAction />
-      </div>
+          <div
+            className="flex items-center justify-center max-w-44 max-h-44 md:max-w-72 md:max-h-80 bg-white 
+            ml-7 md:ml-10 dark:bg-[rgb(16,23,42)]"
+          >
+            <div className="p-2">
+              <h2 className="capitalize text-gray-400 text-sm">
+                featured article
+              </h2>
+              <h1 className="font-bold text-xl md:text-3xl capitalize">
+                {posts[0].title}
+              </h1>
+              <p className="text-xs text-gray-400 mt-1">
+                {fullNameDate(posts[0].createdAt)}
+              </p>
+              <div
+                className="p-1 text-sm line-clamp-4 overflow-hidden"
+                dangerouslySetInnerHTML={{
+                  __html: insertPTag(posts[0].content),
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      )}
 
       <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
         {posts && posts.length > 0 && (
           <div className="flex flex-col gap-6">
-            <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
-            <div className="flex flex-wrap gap-4">
-              {posts.map((post) => (
-                <PostCard key={post._id} post={post} />
-              ))}
+            <div className="flex flex-col gap-2 items-center">
+              <h1 className="font-bold text-2xl">Editor&apos;s Pick</h1>
+              <div className="w-24 h-1 bg-black rounded-sm" />
             </div>
+            {posts.slice(1, 4).map((post) => (
+              <div
+                key={post._id}
+                className="flex items-center gap-3 md:gap-8 mx-auto md:mx-40"
+              >
+                <div className="flex flex-1 justify-end">
+                  <img
+                    src={post.image}
+                    alt=""
+                    className="h-44 w-44 object-cover shadow-md"
+                  />
+                </div>
+                <div className="flex flex-1 flex-col">
+                  <h3 className="capitalize text-gray-400 text-sm">
+                    {post.category}
+                  </h3>
+                  <h2 className="font-semibold text-xl md:text-3xl capitalize">
+                    {post.title}
+                  </h2>
+                  <p className="text-xs text-gray-400 mt-1">
+                    {fullNameDate(posts[0].createdAt)}
+                  </p>
+                  <div
+                    className="text-sm line-clamp-4 overflow-hidden p-1"
+                    dangerouslySetInnerHTML={{
+                      __html: insertPTag(posts[0].content),
+                    }}
+                  />
+                </div>
+              </div>
+            ))}
             <Link
               to={'/search'}
               className="text-lg text-teal-500 hover:underline text-center"
@@ -53,6 +99,87 @@ export default function Home() {
           </div>
         )}
       </div>
+
+      {posts.length > 0 && (
+        <div
+          style={{
+            backgroundImage: `url(${posts[5].image})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+          }}
+          className="h-52 md:h-[440px] flex align-middle my-4 relative"
+        >
+          <div
+            className="flex items-center justify-center max-w-44 max-h-44 md:max-w-72 md:max-h-80 bg-white 
+            dark:bg-[rgb(16,23,42)] absolute top-8 right-16 transform shadow-md"
+          >
+            <div className="p-2">
+              <h2 className="capitalize text-gray-400 text-sm">
+                featured article
+              </h2>
+              <h1 className="font-bold text-xl md:text-3xl capitalize">
+                {posts[0].title}
+              </h1>
+              <p className="text-xs text-gray-400 mt-1">
+                {fullNameDate(posts[0].createdAt)}
+              </p>
+              <div
+                className="p-1 text-sm line-clamp-3 md:line-clamp-4 overflow-hidden"
+                dangerouslySetInnerHTML={{
+                  __html: insertPTag(posts[0].content),
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      )}
+
+      {posts && posts.length > 0 && (
+        <div className="flex w-full my-16 mx-auto md:gap-5 ">
+          <div className="flex flex-col flex-1 max-w-5xl p-1 gap-6">
+            {posts.slice(4, 8).map((post) => (
+              <div
+                key={post._id}
+                className="flex items-center gap-3 md:gap-8 md:mx-14"
+              >
+                <div className="flex justify-end">
+                  <img
+                    src={post.image}
+                    alt=""
+                    className="h-32 w-32 object-cover shadow-md"
+                  />
+                </div>
+
+                <div className="flex flex-1 flex-col p-2">
+                  <h3 className="capitalize text-gray-400 text-sm">
+                    {post.category}
+                  </h3>
+                  <h2 className="font-semibold text-lg md:text-xl capitalize">
+                    {post.title}
+                  </h2>
+                  <p className="text-xs text-gray-400 mt-1">
+                    {fullNameDate(posts[0].createdAt)}
+                  </p>
+                  <div
+                    className="text-sm line-clamp-4 overflow-hidden p-1"
+                    dangerouslySetInnerHTML={{
+                      __html: insertPTag(posts[0].content),
+                    }}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <ul className='p-3 mx-auto'>
+            <h2 className='font-semibold'>tags</h2>
+            {posts.map((post) => (
+              <li key={post._id} className='text-xs md:text-sm'>{post.category}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
