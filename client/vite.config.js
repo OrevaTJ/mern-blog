1b92dcd2cef9ed18699beb0811c7a892a8900a0d
Oreva <khemical51@gmail.com>
2024-03-23 10:58:58 +0100

    config change

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: import.meta.env.PORT || 3000,
+    port: 3000,
     proxy: {
       '/api': {
         // target: 'http://localhost:3001',
