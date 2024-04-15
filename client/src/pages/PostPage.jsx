5b6743cb5af5dbbdba0c440c0d5cb070d5536383
Oreva <khemical51@gmail.com>
2024-04-15 15:23:14 +0100

    add post author details

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,15 +1,16 @@
 import { Button, Spinner } from 'flowbite-react';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import CallToAction from '../components/CallToAction';
 import CommentSection from '../components/CommentSection';
 import PostCard from '../components/PostCard';
+import { httpGetSingleUser } from '../api-services/user';
 
 export default function PostPage() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
+  const [postUser, setPostUser] = useState(null);
   const [recentPosts, setRecentPosts] = useState(null);
 
   // Get single post by slug
@@ -40,6 +41,15 @@ export default function PostPage() {
     fetchPost();
   }, [postSlug]);
 
+  // Effect hook to fetch user data of the post
+  const getUser = useCallback(async () => {
+    post && setPostUser(await httpGetSingleUser(post.userId));
+  }, [post]);
+
+  useEffect(() => {
+    post && getUser();
+  }, [post, getUser]);
+
   // Get recent posts for Recent articles section
   useEffect(() => {
     try {
@@ -67,12 +77,19 @@ export default function PostPage() {
     );
 
   return (
-    <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
+    <main className="p-3 flex flex-col mx-4 min-h-screen">
       {error && (
         <h1 className="text-sm mt-10 p-3 text-center font-serif max-w-2xl mx-auto">
           An Error occurred.
         </h1>
       )}
+
+      <img
+        src={post && post.image}
+        alt={post && post.title}
+        className="max-h-[400px] w-full object-cover rounded-sm"
+      />
+
       <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
         {post && post.title}
       </h1>
@@ -84,11 +101,7 @@ export default function PostPage() {
           {post && post.category}
         </Button>
       </Link>
-      <img
-        src={post && post.image}
-        alt={post && post.title}
-        className="mt-10 p-3 max-h-[600px] w-full object-cover"
-      />
+
       <div className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
         <span className="italic">
@@ -99,9 +112,27 @@ export default function PostPage() {
         className="p-3 max-w-2xl mx-auto w-full post-content"
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       />
-      <div className="max-w-4xl mx-auto w-full">
-        <CallToAction />
-      </div>
+
+      {postUser && (
+        <div className='p-3 max-w-2xl mx-auto'>
+          <h2 className="text-gray-400 text-sm uppercase">about the author</h2>
+          <div className="flex items-center flex-shrink-0 gap-2">
+            <img
+              className="w-20 h-20 rounded-full bg-gray-200"
+              src={postUser.profilePhoto}
+              alt={'#'}
+            />
+            <div>
+              <h1 className='font-semibold text-lg'>{postUser.name}</h1>
+              <h3 className='text-xs mb-2'>@{postUser.username}</h3>
+              <p className='text-xs w-1/2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Corporis sequi excepturi vitae dolorem omnis assumenda.
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
 
       <CommentSection postId={post._id} />
 
