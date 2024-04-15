c0e5d799b606ce53cde5949d25f88c33a067b7a6
Oreva <khemical51@gmail.com>
2024-04-15 15:24:03 +0100

    no change

--- client/src/components/Oauth-button.jsx
+++ client/src/components/Oauth-button.jsx
@@ -8,38 +8,37 @@ import { signInSuccess } from '../redux/user/userSlice';
 import { useNavigate } from 'react-router-dom';
 
 export default function OauthButton() {
-    const auth = getAuth(app)
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const auth = getAuth(app);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-const handleGoogleClick = async () => {
+  const handleGoogleClick = async () => {
     const provider = new GoogleAuthProvider();
-    provider.setCustomParameters({ prompt: 'select_account' })
+    provider.setCustomParameters({ prompt: 'select_account' });
 
     try {
-        const result = await signInWithPopup(auth, provider);
-        const res = await fetch('/api/auth/google', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              name: result.user.displayName,
-              email: result.user.email,
-              profilePhoto: result.user.photoURL,
-            }),
-          });
-    
-          const data = await res.json();
+      const result = await signInWithPopup(auth, provider);
+      const res = await fetch('/api/auth/google', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          profilePhoto: result.user.photoURL,
+        }),
+      });
 
-          if(res.ok) {
-              dispatch(signInSuccess(data)) && navigate('/');
-          }
-    
+      const data = await res.json();
+
+      if (res.ok) {
+        dispatch(signInSuccess(data)) && navigate('/');
+      }
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-}
+  };
 
   return (
     <Button
